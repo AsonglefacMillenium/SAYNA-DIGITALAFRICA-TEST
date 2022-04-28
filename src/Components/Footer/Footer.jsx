f7ab1bd69c7e82af1f2cd9eaf93ea288ad9dc793
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2022-04-29 00:14:16 +0100

    handling responsiveness

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+
+//styles import
+import "./Footer.css";
+
+//assets import
+import Logo1 from "../../Assets/Logo/logo1.png";
+import Logo2 from "../../Assets/Logo/logo2.png";
+import Logo3 from "../../Assets/Logo/logo3.png";
+import Facebook from "../../Assets/Logo/logof.png";
+import Instagram from "../../Assets/Logo/logoi.png";
+import Twitter from "../../Assets/Logo/logot.png";
+import Youtube from "../../Assets/Logo/logoy.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="footer flex flex-column a-j-center">
+      <div className="elements flex">
+        <ul className="flex  a-j-space-between">
+          <li>
+            <Link to="">Mobile App</Link>
+          </li>
+          <li>
+            <Link to="">Politique de Confidentialite</Link>
+          </li>
+          <div className="logo flex">
+          <li>
+            <img src={Logo1} alt="" />
+          </li>
+          <li>
+            <img src={Logo2} alt="" />
+          </li>
+          <li>
+            <img src={Logo3} alt="" />
+          </li>
+          </div>
+          
+          <li>
+          <Link to=""> Contact</Link>
+           
+          </li>
+          <li>
+            <Link to=''>Bog</Link>
+          </li>
+         
+        </ul>
+        <select name="" id="language">
+            <option value="">Lanuage</option>
+            <option value="">French</option>
+            <option value="">English</option>
+          </select>
+      </div>
+      <div className="social flex a-j-center">
+      <a href="">
+      <img src={Facebook} alt="" />
+      </a>
 
-export default Footer
+      <a href="">
+      <img src={Instagram} alt="" />
+      </a>
+
+      <a href="">
+      <img src={Twitter} alt="" />
+      </a>
+       
+       <a href="">
+       <img src={Youtube} alt="" />
+       </a>
+       
+        
+      </div>
+
+      <p>&copy;SynaxMarvel.inc, 2022. We love our users</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,9 @@ import Image2 from "../../Assets/Illustration/img2.png";
 import Image3 from "../../Assets/Illustration/img3.png";
 import Image4 from "../../Assets/Illustration/img4.png";
 import Image5 from "../../Assets/Illustration/img5.png";
-import Image6 from '../../Assets/Illustration/img6.png'
+import Image6 from "../../Assets/Illustration/img6.png";
+import Audio from "../../Assets/audio1.mp3";
+import Footer from "../../Components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -26,7 +28,7 @@ const Home = () => {
               <Link to="/">
                 <Button
                   type="primary"
-                  text="Voir Les Heros"
+                  text="Voir Le Heros"
                   padding="0.7rem 2rem"
                   borderThickness="2"
                   fontSize="20px"
@@ -37,7 +39,7 @@ const Home = () => {
               <Link to="/">
                 <Button
                   type="secondary"
-                  text="Voir Les Heros"
+                  text="Voir Les Adversaire"
                   padding="0.7rem 2rem"
                   borderThickness="2"
                   fontSize="20px"
@@ -55,22 +57,28 @@ const Home = () => {
       <div className="spidercards flex-column full-width a-j-center">
         <div className="spidercard__head">
           <h1>LES SPIDERMANS AU CINEMA</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Spiderman a pu bénéficier de reboot qui ont permis d’adapter ses
+            histoires pour chaque génération !
+          </p>
         </div>
 
         <div className="spidercards__body flex a-j-center ">
-          <div className="card flex-column flex">
+          <div className="card flex-column flex flex-wrap">
             <img src={Image3} alt="" />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
-              sit alias repellendus, obcaecati similique pariatur? Consequatur
-              repellat provident et numquam sequi quibusdam? Doloremque mollitia
-              porro commodi odit dicta architecto non.
+              Trois films Spiderman sont sortis en 2002, 2004 et 2007. Ils sont
+              produits par Columbia Pictures et Spiderman est incarné par Tobey
+              Maguire. Un quatrième était prévu, mais sera finalement annulé
+              suite à des désaccords entre Sam Raimi (le réalisateur) et Sony.
+              Cette première trilogie a connu un grand succès auprès des fans et
+              du grand public. L’épisode 3 reste à ce jour un des films les plus
+              chers de l’histoire du cinéma.
             </p>
             <Link to="/">
               <Button
                 type="primary"
-                text="Voir Les Heros"
+                text="Voir Les adversaires"
                 padding="0.7rem 2rem"
                 borderThickness="2"
                 fontSize="20px"
@@ -82,15 +90,19 @@ const Home = () => {
           <div className="card flex-column flex">
             <img src={Image4} alt="" />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
-              sit alias repellendus, obcaecati similique pariatur? Consequatur
-              repellat provident et numquam sequi quibusdam? Doloremque mollitia
-              porro commodi odit dicta architecto non.
+              The Amazing Spiderman est sorti en 2012, c’est Andrew Garfield qui
+              incarne le héros et Marc Webb à la réalisation. Un deuxième volet,
+              The Amazing Spider-Man : Le destin d'un héros, est sorti en 2014
+              et a reçu des critiques très mitigées. Un troisième et quatrième
+              film étaient prévus pour 2016 et 2018. Finalement, Marvel et Sony
+              trouvent un accord pour introduire Spiderman dans l'Univers
+              cinématographique Marvel et abandonne ainsi The Amazing
+              Spider-Man.
             </p>
             <Link to="/">
               <Button
                 type="primary"
-                text="Voir Les Heros"
+                text="Voir Les adversaires"
                 padding="0.7rem 2rem"
                 borderThickness="2"
                 fontSize="20px"
@@ -102,15 +114,23 @@ const Home = () => {
           <div className="card flex-column flex">
             <img src={Image2} alt="" />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
-              sit alias repellendus, obcaecati similique pariatur? Consequatur
-              repellat provident et numquam sequi quibusdam? Doloremque mollitia
-              porro commodi odit dicta architecto non.
+              Spiderman, désormais incarné par Tom Holland, apparaît pour la
+              première fois dans l’univers Marvel avec Captain America : Civil
+              War, sorti en 2016. Par la suite, une trilogie de films lui est
+              consacrée :
+              <ul>
+                <li>Spiderman : Homecoming (2017)</li>
+                <li>Spiderman : Far From Home (2019)</li>
+
+                <li>Spiderman : No Way Home (2021)</li>
+              </ul>
+              Le héro est également présent dans Avengers Infinity War et
+              Avengers End Game, combattant au côté des Avengers.
             </p>
             <Link to="/">
               <Button
                 type="primary"
-                text="Voir Les Heros"
+                text="Voir Les adversaires"
                 padding="0.7rem 2rem"
                 borderThickness="2"
                 fontSize="20px"
@@ -128,41 +148,48 @@ const Home = () => {
         </div>
         <div className="form__box flex-column a-j-space-between">
           <h1>DEVIENS UN SPIDERMAN!</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Rempli le formulaire ci-dessous afin de recevoir ton propre costume.
+          </p>
           <form action=" flex flex-column">
-            <input type="text" placeholder="" />
-            <input type="text" placeholder="" />
-            <input type="text" placeholder="" />
-            <input type="text" placeholder="" />
-           <button type="submit">
-               Envoyez
-           </button>
-            
+            <input type="text" placeholder="Nom" />
+            <input type="text" placeholder="Prenom" />
+            <input type="text" placeholder="Taille (cm)" />
+            <input type="text" placeholder="Poids (kg)" />
+            <input type="email" placeholder="Adresse (email)" />
+            <button type="submit">Envoyez</button>
           </form>
         </div>
       </div>
 
       <div className="black__red flex a-j-space-between">
-      <div className="black__red__left flex-column a-j-space-between">
-          <h1>DEVIENS UN SPIDERMAN!</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className="black__red__left flex-column a-j-space-between">
+          <h1>Inscrit toi à la newsletter !</h1>
+          <p>
+            L’univers de l’araignée justicière te passionne ? Reste au courant
+            des dernières actualités grâce à notre newsletter !
+          </p>
           <form action=" flex flex-column">
-            <input type="text" placeholder="" />
-            <input type="text" placeholder="" />
-            <div className="checkbox flex a-j-space-between">
-                <input type="checkbox" name="" id="" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <input type="text" placeholder="Nom Prenom" />
+            <input type="text" placeholder=" Adresse Email" />
+            <div className="checkbox flex ">
+              <input type="checkbox" name="" id="" />
+              <p>
+                Je reconnais accepter la politique de confidentialité de Sayna X
+                Marvel.
+              </p>
             </div>
-           <button type="submit">
-               Envoyez
-           </button>
-            
+            <button type="submit" >S'inscrire a la newsletter</button>
           </form>
         </div>
         <div className="image__right ">
           <img src={Image6} alt="" />
         </div>
       </div>
+
+      <audio src={Audio}>audio</audio>
+
+      <Footer />
     </div>
   );
 };
